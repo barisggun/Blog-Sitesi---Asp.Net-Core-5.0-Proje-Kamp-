1e7ff31f445d1d3b75fac7e3b8456c75fcb66f18
Barış Gün <108459412+bgun7@users.noreply.github.com>
2023-05-11 23:33:34 +0300

    Dashboard İstatistikleri Eklendi

--- Asp.NetProjeKampi/Controllers/DashboardController.cs
+++ Asp.NetProjeKampi/Controllers/DashboardController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Asp.NetProjeKampi.Controllers
@@ -8,8 +9,13 @@ namespace Asp.NetProjeKampi.Controllers
         [AllowAnonymous]
         public IActionResult Index()
         {
-
+            Context c = new Context();
+            ViewBag.v1=c.Blogs.Count().ToString();
+            ViewBag.v2=c.Blogs.Where(x=>x.WriterID==1).Count();
+            ViewBag.v3=c.Categories.Count().ToString();
             return View();
         }
+
+
     }
 }
